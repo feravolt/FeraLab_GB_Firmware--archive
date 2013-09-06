1e2daacda38a6bd1e7c0bb455c1f78cb0006b3ad
feravolt <fera.volt@gmail.com>
2013-09-06 09:57:00 +0300

    Revert mtd hack

--- arch/arm/mach-msm/nand_partitions.c
+++ arch/arm/mach-msm/nand_partitions.c
@@ -1,18 +1,43 @@
-/* FeraLab */
+/* arch/arm/mach-msm/nand_partitions.c
+ *
+ * Code to extract partition information from ATAG set up by the
+ * bootloader.
+ *
+ * Copyright (C) 2007 Google, Inc.
+ * Copyright (c) 2008-2009, Code Aurora Forum. All rights reserved.
+ * Author: Brian Swetland <swetland@google.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
+
 #include <asm/mach/flash.h>
 #include <linux/io.h>
+
 #include <asm/setup.h>
+
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
+
 #include <mach/msm_iomap.h>
+
 #include <mach/board.h>
 #include "smd_private.h"
-#define ATAG_MSM_PARTITION 0x4d534D70
-#define MSM_MAX_PARTITIONS 8
+
+/* configuration tags specific to msm */
+
+#define ATAG_MSM_PARTITION 0x4d534D70 /* MSMp */
 
 struct msm_ptbl_entry {
 	char name[16];
@@ -21,8 +46,11 @@ struct msm_ptbl_entry {
 	__u32 flags;
 };
 
+#define MSM_MAX_PARTITIONS 8
+
 static struct mtd_partition msm_nand_partitions[MSM_MAX_PARTITIONS];
 static char msm_nand_names[MSM_MAX_PARTITIONS * 16];
+
 extern struct flash_platform_data msm_nand_data;
 
 static int __init parse_tag_msm_partition(const struct tag *tag)
@@ -66,16 +94,17 @@ __tagtable(ATAG_MSM_PARTITION, parse_tag_msm_partition);
 #define FLASH_PART_MAGIC1     0x55EE73AA
 #define FLASH_PART_MAGIC2     0xE35EBDDB
 #define FLASH_PARTITION_VERSION   0x3
+
 #define LINUX_FS_PARTITION_NAME  "0:EFS2APPS"
 
 struct flash_partition_entry {
 	char name[16];
-	u32 offset;
-	u32 length;
+	u32 offset;	/* Offset in blocks from beginning of device */
+	u32 length;	/* Length of the partition in blocks */
 	u8 attrib1;
 	u8 attrib2;
 	u8 attrib3;
-	u8 which_flash;
+	u8 which_flash;	/* Numeric ID (first = 0, second = 1) */
 };
 struct flash_partition_table {
 	u32 magic1;
@@ -88,60 +117,60 @@ struct flash_partition_table {
 static struct mtd_partition nand_partitions[] = {
         {
                 .name           = "appslog",
-                .size           = 0x00000022 >> 1,
-                .offset         = 0x00001fde >> 1,
+                .size           = 0x00000044 >> 1,
+                .offset         = 0x00003fbc >> 1,
         }, {
                 .name           = "cache",
-                .size           = 0x0000037a >> 1,
-                .offset         = 0x00001c64 >> 1,
+                .size           = 0x000006f4 >> 1,
+                .offset         = 0x000038c8 >> 1,
         }, {
                 .name           = "system",
-                .size           = 0x00000b05 >> 1,
-                .offset         = 0x000002d7 >> 1,
+                .size           = 0x0000160a >> 1,
+                .offset         = 0x000005ae >> 1,
         }, {
                 .name           = "userdata",
-                .size           = 0x00000e88 >> 1,
-                .offset         = 0x00000ddc >> 1,
+                .size           = 0x00001d10 >> 1,
+                .offset         = 0x00001bb8 >> 1,
         }, {
                 .name           = "bootloader",
                 .size           = 0x00000016,
                 .offset         = 0,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "amss",
                 .size           = 0x000000bf,
                 .offset         = 0x00000016,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "amss_fs",
                 .size           = 0x00000058,
                 .offset         = 0x000000d5,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "fota0",
                 .size           = 0x00000022,
                 .offset         = 0x0000012d,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "fota1",
                 .size           = 0x00000022,
                 .offset         = 0x0000014f,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "recovery",
                 .size           = 0x00000062,
                 .offset         = 0x00000171,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "dsp1",
                 .size           = 0x000000a2,
                 .offset         = 0x000001d3,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }, {
                 .name           = "boot",
                 .size           = 0x00000062,
                 .offset         = 0x00000275,
-                .mask_flags     = MTD_WRITEABLE,
+                .mask_flags     = MTD_WRITEABLE,  /* force read-only */
         }
 };
 
@@ -166,6 +195,7 @@ static int get_nand_partitions(void)
 		msm_nand_data.nr_parts = ARRAY_SIZE(nand_partitions);
 		msm_nand_data.parts = nand_partitions;
 		return 0;
+		//return -ENOENT;
 	}
 
 	if ((partition_table->magic1 != (u32) FLASH_PART_MAGIC1) ||
@@ -181,14 +211,21 @@ static int get_nand_partitions(void)
 
 	msm_nand_data.nr_parts = 0;
 
+	/* Get the LINUX FS partition info */
 	for (part = 0; part < partition_table->numparts; part++) {
 		part_entry = &partition_table->part_entry[part];
 
+		/* Find a match for the Linux file system partition */
 		if (strcmp(part_entry->name, LINUX_FS_PARTITION_NAME) == 0) {
 			strcpy(name, part_entry->name);
 			ptn->name = name;
+
+			/*TODO: Get block count and size info */
 			ptn->offset = part_entry->offset;
 
+			/* For SMEM, -1 indicates remaining space in flash,
+			 * but for MTD it is 0
+			 */
 			if (part_entry->length == (u32)-1)
 				ptn->size = 0;
 			else
@@ -206,7 +243,9 @@ static int get_nand_partitions(void)
 	}
 
 	printk(KERN_WARNING "%s: no partition table found!", __func__);
+
 	return -ENODEV;
 }
 
 device_initcall(get_nand_partitions);
+
