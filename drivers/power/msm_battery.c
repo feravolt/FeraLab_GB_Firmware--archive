702caaf1da806d530d0782392033a5bfb4d93e37
feravolt <fera.volt@gmail.com>
2013-09-19 22:50:00 +0300

    Update battery status upon resume

--- drivers/power/msm_battery.c
+++ drivers/power/msm_battery.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -826,6 +826,7 @@ void msm_batt_late_resume(struct early_suspend *h)
 		return;
 	}
 
+	msm_batt_update_psy_status();
 	pr_info("%s: exit\n", __func__);
 }
 #endif
