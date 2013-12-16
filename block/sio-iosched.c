d3b853edbe5decc8229de6b594bba701ff4448da
feravolt <fera.volt@gmail.com>
2013-12-16 15:46:02 +0200

    Fix SIO i/o scheduler

--- block/sio-iosched.c
+++ block/sio-iosched.c
@@ -1,22 +1,3 @@
-/*
- * Simple IO scheduler plus
- * Based on Noop, Deadline and V(R) IO schedulers.
- *
- * Copyright (C) 2012 Miguel Boton <mboton@gmail.com>
- *           (C) 2013 Boy Petersen <boypetersen@gmail.com>
- *
- *
- * This algorithm does not do any kind of sorting, as it is aimed for
- * aleatory access devices, but it does some basic merging. We try to
- * keep minimum overhead to achieve low latency.
- *
- * Asynchronous and synchronous requests are not treated separately, but
- * we relay on deadlines to ensure fairness.
- *
- * The plus version fixes writes_starved not being initialized on startup
- * and also modifies the write starvation counting logic.
- *
- */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
@@ -26,7 +7,6 @@
 
 enum { ASYNC, SYNC };
 
-/* Tunables */
 static const int sync_read_expire = (HZ / 16) * 9;	/* max time before a sync read is submitted. */
 static const int sync_write_expire = (HZ / 2) * 5;	/* max time before a sync write is submitted. */
 static const int async_read_expire = HZ * 4;	/* ditto for async, these limits are SOFT! */
@@ -35,16 +15,10 @@ static const int writes_starved = 4;		/* max times reads can starve a write */
 static const int fifo_batch     = 1;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
-/* Elevator data */
 struct sio_data {
-	/* Request queues */
 	struct list_head fifo_list[2][2];
-
-	/* Attributes */
 	unsigned int batched;
 	unsigned int starved;
-
-	/* Settings */
 	int fifo_expire[2][2];
 	int fifo_batch;
 	int writes_starved;
@@ -54,10 +28,6 @@ static void
 sio_merged_requests(struct request_queue *q, struct request *rq,
 		    struct request *next)
 {
-	/*
-	 * If next expires before rq, assign its expire time to rq
-	 * and move into next position (next will be deleted) in fifo.
-	 */
 	if (!list_empty(&rq->queuelist) && !list_empty(&next->queuelist)) {
 		if (time_before(rq_fifo_time(next), rq_fifo_time(rq))) {
 			list_move(&rq->queuelist, &next->queuelist);
@@ -65,7 +35,6 @@ sio_merged_requests(struct request_queue *q, struct request *rq,
 		}
 	}
 
-	/* Delete next request */
 	rq_fifo_clear(next);
 }
 
@@ -75,11 +44,6 @@ sio_add_request(struct request_queue *q, struct request *rq)
 	struct sio_data *sd = q->elevator->elevator_data;
 	const int sync = rq_is_sync(rq);
 	const int data_dir = rq_data_dir(rq);
-
-	/*
-	 * Add request to the proper fifo list and set its
-	 * expire time.
-	 */
 	rq_set_fifo_time(rq, jiffies + sd->fifo_expire[sync][data_dir]);
 	list_add_tail(&rq->queuelist, &sd->fifo_list[sync][data_dir]);
 }
@@ -88,8 +52,6 @@ static int
 sio_queue_empty(struct request_queue *q)
 {
 	struct sio_data *sd = q->elevator->elevator_data;
-
-	/* Check if fifo lists are empty */
 	return list_empty(&sd->fifo_list[SYNC][READ]) && list_empty(&sd->fifo_list[SYNC][WRITE]) &&
 	       list_empty(&sd->fifo_list[ASYNC][READ]) && list_empty(&sd->fifo_list[ASYNC][WRITE]);
 }
@@ -102,11 +64,8 @@ sio_expired_request(struct sio_data *sd, int sync, int data_dir)
 
 	if (list_empty(list))
 		return NULL;
-
-	/* Retrieve request */
 	rq = rq_entry_fifo(list->next);
 
-	/* Request has expired */
 	if (time_after_eq(jiffies, rq_fifo_time(rq)))
 		return rq;
 
@@ -117,12 +76,6 @@ static struct request *
 sio_choose_expired_request(struct sio_data *sd)
 {
 	struct request *rq;
-
-	/*
-	 * Check expired requests.
-	 * Asynchronous requests have priority over synchronous.
-	 * Write requests have priority over read.
-	 */
 	rq = sio_expired_request(sd, ASYNC, WRITE);
 	if (rq)
 		return rq;
@@ -137,7 +90,6 @@ sio_choose_expired_request(struct sio_data *sd)
 	if (rq)
 		return rq;
 
-
 	return NULL;
 }
 
@@ -147,11 +99,6 @@ sio_choose_request(struct sio_data *sd, int data_dir)
 	struct list_head *sync = sd->fifo_list[SYNC];
 	struct list_head *async = sd->fifo_list[ASYNC];
 
-	/*
-	 * Retrieve request from available fifo list.
-	 * Synchronous requests have priority over asynchronous.
-	 * Read requests have priority over write.
-	 */
 	if (!list_empty(&sync[data_dir]))
 		return rq_entry_fifo(sync[data_dir].next);
 	if (!list_empty(&async[data_dir]))
@@ -168,11 +115,6 @@ sio_choose_request(struct sio_data *sd, int data_dir)
 static inline void
 sio_dispatch_request(struct sio_data *sd, struct request *rq)
 {
-
-	/*
-	 * Remove the request from the fifo list
-	 * and dispatch it.
-	 */
 	rq_fifo_clear(rq);
 	elv_dispatch_add_tail(rq->q, rq);
 
@@ -194,16 +136,11 @@ sio_dispatch_requests(struct request_queue *q, int force)
 	struct request *rq = NULL;
 	int data_dir = READ;
 
-	/*
-	 * Retrieve any expired request after a batch of
-	 * sequential requests.
-	 */
 	if (sd->batched > sd->fifo_batch) {
 		sd->batched = 0;
 		rq = sio_choose_expired_request(sd);
 	}
 
-	/* Retrieve request */
 	if (!rq) {
 		if (sd->starved > sd->writes_starved)
 			data_dir = WRITE;
@@ -213,9 +150,7 @@ sio_dispatch_requests(struct request_queue *q, int force)
 			return 0;
 	}
 
-	/* Dispatch request */
 	sio_dispatch_request(sd, rq);
-
 	return 1;
 }
 
@@ -229,7 +164,6 @@ sio_former_request(struct request_queue *q, struct request *rq)
 	if (rq->queuelist.prev == &sd->fifo_list[sync][data_dir])
 		return NULL;
 
-	/* Return former request */
 	return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
@@ -243,7 +177,6 @@ sio_latter_request(struct request_queue *q, struct request *rq)
 	if (rq->queuelist.next == &sd->fifo_list[sync][data_dir])
 		return NULL;
 
-	/* Return latter request */
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
@@ -251,19 +184,14 @@ static void *
 sio_init_queue(struct request_queue *q)
 {
 	struct sio_data *sd;
-
-	/* Allocate structure */
 	sd = kmalloc_node(sizeof(*sd), GFP_KERNEL, q->node);
 	if (!sd)
 		return NULL;
 
-	/* Initialize fifo lists */
 	INIT_LIST_HEAD(&sd->fifo_list[SYNC][READ]);
 	INIT_LIST_HEAD(&sd->fifo_list[SYNC][WRITE]);
 	INIT_LIST_HEAD(&sd->fifo_list[ASYNC][READ]);
 	INIT_LIST_HEAD(&sd->fifo_list[ASYNC][WRITE]);
-
-	/* Initialize data */
 	sd->batched = 0;
 	sd->fifo_expire[SYNC][READ] = sync_read_expire;
 	sd->fifo_expire[SYNC][WRITE] = sync_write_expire;
@@ -271,7 +199,6 @@ sio_init_queue(struct request_queue *q)
 	sd->fifo_expire[ASYNC][WRITE] = async_write_expire;
 	sd->fifo_batch = fifo_batch;
 	sd->writes_starved = writes_starved;
-
 	return sd;
 }
 
@@ -279,19 +206,12 @@ static void
 sio_exit_queue(struct elevator_queue *e)
 {
 	struct sio_data *sd = e->elevator_data;
-
 	BUG_ON(!list_empty(&sd->fifo_list[SYNC][READ]));
 	BUG_ON(!list_empty(&sd->fifo_list[SYNC][WRITE]));
 	BUG_ON(!list_empty(&sd->fifo_list[ASYNC][READ]));
 	BUG_ON(!list_empty(&sd->fifo_list[ASYNC][WRITE]));
-
-	/* Free structure */
 	kfree(sd);
 }
-
-/*
- * sysfs code
- */
 
 static ssize_t
 sio_var_show(int var, char *page)
@@ -363,7 +283,7 @@ static struct elv_fs_entry sio_attrs[] = {
 	__ATTR_NULL
 };
 
-static struct elevator_type iosched_sioplus = {
+static struct elevator_type iosched_sio = {
 	.ops = {
 		.elevator_merge_req_fn		= sio_merged_requests,
 		.elevator_dispatch_fn		= sio_dispatch_requests,
@@ -376,28 +296,25 @@ static struct elevator_type iosched_sioplus = {
 	},
 
 	.elevator_attrs = sio_attrs,
-	.elevator_name = "sioplus",
+	.elevator_name = "sio",
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init sioplus_init(void)
+static int __init sio_init(void)
 {
-	/* Register elevator */
-	elv_register(&iosched_sioplus);
-
+	elv_register(&iosched_sio);
 	return 0;
 }
 
-static void __exit sioplus_exit(void)
+static void __exit sio_exit(void)
 {
-	/* Unregister elevator */
-	elv_unregister(&iosched_sioplus);
+	elv_unregister(&iosched_sio);
 }
 
-module_init(sioplus_init);
-module_exit(sioplus_exit);
+module_init(sio_init);
+module_exit(sio_exit);
 
-MODULE_AUTHOR("Miguel Boton");
+MODULE_AUTHOR("People");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Simple IO scheduler plus");
+MODULE_DESCRIPTION("Simple IO scheduler");
 
