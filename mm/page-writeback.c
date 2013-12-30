048e324cec3287788bc48c115b363ce154c962ae
feravolt <fera.volt@gmail.com>
2013-12-30 19:26:53 +0200

    Tune dirty ratios & update config

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -66,7 +66,7 @@ static inline long sync_writeback_pages(void)
 /*
  * Start background writeback (via pdflush) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 2;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -83,7 +83,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 20;
+int vm_dirty_ratio = 4;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
@@ -94,12 +94,12 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 10 * 100; /* sentiseconds */
+unsigned int dirty_writeback_interval = 5 * 100; /* sentiseconds */
 
 /*
  * The longest time for which data is allowed to remain dirty
  */
-unsigned int dirty_expire_interval = 20 * 100; /* sentiseconds */
+unsigned int dirty_expire_interval = 2 * 100; /* sentiseconds */
 
 /*
  * Flag that makes the machine dump writes/reads and block dirtyings.
