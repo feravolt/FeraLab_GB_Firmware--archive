4e8744dcfff1a821a96cba0042d62dadbce8768a
feravolt <fera.volt@gmail.com>
2014-01-04 20:18:37 +0200

    Add ramdisk.. Automize compilation

---
+++ tools/input/ramdisk-folder/engine.sh
@@ -0,0 +1,30 @@
+### FeraLab ###
+
+echo "FeraDroid Engine"
+echo "By FeraVolt."
+echo "Initialization..."
+
+export PATH=/sbin
+chmod 777 /sbin/sysrw
+chmod 777 /sbin/sysro
+/sbin/sysrw
+rm -f /system/lib/modules/ar6000.ko
+cp /modules/wifi.ko /system/lib/modules/wifi.ko
+chmod 666 /system/lib/modules/wifi.ko
+mount -t debugfs debugfs /sys/kernel/debug
+mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+echo 0 > /sys/kernel/debug/msm_fb/0/vsync_enable
+mount -o bind /system/engine /engine
+chmod 777 /engine
+chmod 777 /cache
+cd /engine
+/sbin/sysrw
+mkdir sysinit
+cd
+mount -o bind /system/etc/init.d /system/engine/sysinit
+chmod -R 777 /engine/*
+chmod -R 777 /engine/sysinit/*
+chmod -R 777 /engine/tweaks/*
+chmod -R 777 /system/usr/vendor/prop/*
+/system/xbin/run-parts /system/etc/init.d
+
