cc1ffe54cf2d155e4567e5232a3740351e896400
FeraVolt <fera.volt@gmail.com>
2014-01-12 00:07:44 +0200

    Improve CPU power-saving

--- kernel/kernel/time/tick-sched.c
+++ kernel/kernel/time/tick-sched.c
@@ -270,7 +270,9 @@ void tick_nohz_stop_sched_tick(int inidle)
 	next_jiffies = get_next_timer_interrupt(last_jiffies);
 	delta_jiffies = next_jiffies - last_jiffies;
 
-	if (rcu_needs_cpu(cpu) || printk_needs_cpu(cpu))
+	if (rcu_needs_cpu(cpu) || printk_needs_cpu(cpu)) || 
+		arch_needs_cpu(cpu) || this_cpu_load()) {
+		next_jiffies = last_jiffies + 1;
 		delta_jiffies = 1;
 	/*
 	 * Do not stop the tick, if we are only one off
