f9a0edb017e04ffd39b28be2912e9d3cd0208f1a
feravolt <fera.volt@gmail.com>
2014-04-16 15:31:15 +0300

    Fix undervolt & increse touch screen sensivity

--- kernel/arch/arm/mach-msm/board-es209ra.c
+++ kernel/arch/arm/mach-msm/board-es209ra.c
@@ -568,7 +568,7 @@ static struct spi_board_info msm_spi_board_info[] __initdata = {
 		.irq		= INT_ES209RA_GPIO_TOUCHPAD,
 		.bus_num	= 0,
 		.chip_select	= 0,
-		.max_speed_hz	= 2000000,
+		.max_speed_hz	= 3000000,
 		.platform_data  = &es209ra_touch_data,
 	}
 };
