83be26a71d6757bad35843bf908efa85e6e4a414
feravolt <fera.volt@gmail.com>
2014-04-30 17:13:13 +0300

    Apply ultimate Linaro optimization trics which doesn't compile yet.

--- kernel/arch/arm/boot/compressed/misc.c
+++ kernel/arch/arm/boot/compressed/misc.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * misc.c
  * 
  * This is a collection of several routines from gzip-1.0.3 
@@ -133,7 +133,7 @@ void __memzero (__ptr_t s, size_t n)
 		*u.ucp++ = 0;
 }
 
-static inline __ptr_t memcpy(__ptr_t __dest, __const __ptr_t __src,
+inline __ptr_t memcpy(__ptr_t __dest, __const __ptr_t __src,
 			    size_t __n)
 {
 	int i = 0;
