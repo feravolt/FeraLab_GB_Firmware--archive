34d91d1b37f303fd7c93ce5b31ea85ab04615a37
Fera Volt <fera.volt@gmail.com>
2014-05-15 15:11:04 +0300

    Moore RAM (Y)

--- kernel/arch/arm/nwfpe/fpopcode.c
+++ kernel/arch/arm/nwfpe/fpopcode.c
@@ -1,24 +1,3 @@
-/*
-    NetWinder Floating Point Emulator
-    (c) Rebel.COM, 1998,1999
-
-    Direct questions, comments to Scott Bambrough <scottb@netwinder.org>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
-
 #include "fpa11.h"
 #include "softfloat.h"
 #include "fpopcode.h"
@@ -60,4 +39,28 @@ const float32 float32Constant[] = {
 	0x3f000000,		/* single 0.5 */
 	0x41200000		/* single 10.0 */
 };
+
+static const unsigned short aCC[16] = {
+	0xF0F0,			// EQ == Z set
+	0x0F0F,			// NE
+	0xCCCC,			// CS == C set
+	0x3333,			// CC
+	0xFF00,			// MI == N set
+	0x00FF,			// PL
+	0xAAAA,			// VS == V set
+	0x5555,			// VC
+	0x0C0C,			// HI == C set && Z clear
+	0xF3F3,			// LS == C clear || Z set
+	0xAA55,			// GE == (N==V)
+	0x55AA,			// LT == (N!=V)
+	0x0A05,			// GT == (!Z && (N==V))
+	0xF5FA,			// LE == (Z || (N!=V))
+	0xFFFF,			// AL always
+	0			// NV
+};
+
+unsigned int checkCondition(const unsigned int opcode, const unsigned int ccodes)
+{
+	return (aCC[opcode >> 28] >> (ccodes >> 28)) & 1;
+}
 
