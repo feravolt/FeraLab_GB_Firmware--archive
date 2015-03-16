b6cb964c30007f35a6be54979a52a9709a33ba10
FeraVolt <fera.volt@gmail.com>
2015-03-16 22:23:44 +0500

    Optimized RND framework. Updated memset lib from CM New AROMA theme. Misc.

--- kernel/drivers/char/random.c
+++ kernel/drivers/char/random.c
@@ -28,9 +28,9 @@
 #define EXTRACT_SIZE 10
 
 
-static int random_read_wakeup_thresh = 128;
+static int random_read_wakeup_thresh = 512;
 
-static int random_write_wakeup_thresh = 256;
+static int random_write_wakeup_thresh = 1024;
 
 static int trickle_thresh __read_mostly = INPUT_POOL_WORDS * 28;
 
@@ -549,17 +549,12 @@ static ssize_t random_write(struct file *file, const char __user *buffer,
 			    size_t count, loff_t *ppos)
 {
 	size_t ret;
-	struct inode *inode = file->f_path.dentry->d_inode;
-
 	ret = write_pool(&blocking_pool, buffer, count);
 	if (ret)
 		return ret;
 	ret = write_pool(&nonblocking_pool, buffer, count);
 	if (ret)
 		return ret;
-
-	inode->i_mtime = current_fs_time(inode->i_sb);
-	mark_inode_dirty(inode);
 	return (ssize_t)count;
 }
 
